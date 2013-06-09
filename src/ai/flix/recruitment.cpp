878a7d745dcc3320356ad95a88198ea5d37414b5
flix <fehlxb@gmail.com>
2013-06-09 04:38:45 +0300

    Add Multiple Leader support to recruitment.

--- src/ai/flix/recruitment.cpp
+++ src/ai/flix/recruitment.cpp
@@ -10,26 +10,23 @@
    but WITHOUT ANY WARRANTY.
 
    See the COPYING file for more details.
-*/
-
-/**
- * Experimental Recruitment Engine by Flix
- *
- * @file
  */
 
-
-#include "../../resources.hpp"
-#include "../../map.hpp"
-#include "../../unit_map.hpp"
-#include "../../unit_types.hpp"
-#include "../../log.hpp"
-#include "../../team.hpp"
-
-#include "../composite/rca.hpp"
-#include "../actions.hpp"
+/**
+ * @file
+ * Recruitment Engine by flix
+ */
 
 #include "recruitment.hpp"
+
+#include "../actions.hpp"
+#include "../composite/rca.hpp"
+#include "../../log.hpp"
+#include "../../map.hpp"
+#include "../../resources.hpp"
+#include "../../team.hpp"
+#include "../../unit_map.hpp"
+#include "../../unit_types.hpp"
 
 #include <boost/foreach.hpp>
 
@@ -40,7 +37,7 @@ static lg::log_domain log_ai_flix("ai/flix");
 
 #ifdef _MSC_VER
 #pragma warning(push)
-//silence "inherits via dominance" warnings
+// silence "inherits via dominance" warnings
 #pragma warning(disable:4250)
 #endif
 
@@ -48,125 +45,227 @@ namespace ai {
 
 namespace flix_recruitment {
 
-	recruitment::recruitment( rca_context &context , const config &cfg ):
-		candidate_action(context, cfg){
+recruitment::recruitment(rca_context &context, const config &cfg)
+		: candidate_action(context, cfg) { }
 
+double recruitment::evaluate() {
+	std::vector<unit_map::unit_iterator> leaders = resources::units->find_leaders(get_side());
+
+	BOOST_FOREACH(const unit_map::unit_iterator &leader, leaders) {
+		if (leader == resources::units->end()) {
+			return BAD_SCORE;
+		}
+
+		std::set<map_location> checked_hexes;
+		const map_location &keep = leader->get_location();
+		checked_hexes.insert(keep);
+
+		if (resources::game_map->is_keep(leader->get_location()) &&
+				count_free_hexes_in_castle(leader->get_location(), checked_hexes) != 0) {
+			return get_score();
+		}
 	}
-	double recruitment::evaluate(){
-		std::vector<unit_map::unit_iterator> leaders = resources::units->find_leaders(get_side());
 
-		BOOST_FOREACH(unit_map::unit_iterator &leader, leaders){
-			if (leader == resources::units->end()) {
-				return BAD_SCORE;
-			}
+	return BAD_SCORE;
+}
 
-			std::set<map_location> checked_hexes;
-			const map_location &keep = leader->get_location();
-			checked_hexes.insert(keep);
+void recruitment::execute() {
+	LOG_AI_FLIX<< "Flix recruitment begin! \n";
 
-			if (resources::game_map->is_keep(leader->get_location()) && count_free_hexes_in_castle(leader->get_location(), checked_hexes) != 0) {
-				return get_score();
+	/*
+	 * Step 0: Check which leaders can recruit and collect them in leader_data.
+	 *         Also initialize some other stuff.
+	 */
+
+	const std::vector<unit_map::unit_iterator>& leaders =
+			resources::units->find_leaders(get_side());
+
+	// this is the central datastructure with all score_tables in it.
+	std::vector<data> leader_data;
+	// a set of all possible recruits
+	std::set<std::string> global_recruits;
+	int cheapest_unit_cost;
+
+	BOOST_FOREACH(const unit_map::unit_iterator& leader_it, leaders) {
+		const unit& leader = *leader_it;
+		const map_location &keep = leader.get_location();
+		if(!resources::game_map->is_keep(keep)) {
+			DBG_AI_FLIX << "Leader " << leader.name() << " is not on keep. \n";
+			continue;
+		}
+		std::set<map_location> checked_hexes;
+		checked_hexes.insert(keep);
+		if(count_free_hexes_in_castle(keep, checked_hexes) <= 0) {
+			DBG_AI_FLIX << "Leader " << leader.name() << "is on keep but no hexes are free \n";
+			continue;
+		}
+
+		// leader can recruit.
+
+		data data(leader);
+
+		// add team recruits
+		BOOST_FOREACH(const std::string& recruit, current_team().recruits()) {
+			data.recruits.insert(recruit);
+			data.scores[recruit] = 0.0;
+			data.limits[recruit] = 99999;
+			global_recruits.insert(recruit);
+			const unit_type* const info = unit_types.find(recruit);
+			if (info->cost() < cheapest_unit_cost) {
+				cheapest_unit_cost = info->cost();
 			}
 		}
 
-		return BAD_SCORE;
+		// add extra recruits
+		BOOST_FOREACH(const std::string& recruit, leader.recruits()) {
+			data.recruits.insert(recruit);
+			data.scores[recruit] = 0.0;
+			data.limits[recruit] = 99999;
+			global_recruits.insert(recruit);
+			const unit_type* const info = unit_types.find(recruit);
+			if (info->cost() < cheapest_unit_cost) {
+				cheapest_unit_cost = info->cost();
+			}
+		}
+
+		leader_data.push_back(data);
 	}
 
-	void recruitment::execute() {
-		LOG_AI_FLIX << "Flix recruitment begin! \n";
+	if (leader_data.empty()) {
+		DBG_AI_FLIX << "No leader available for recruiting. \n";
+		return;  // This CA is going to be blacklisted for this turn.
+	}
 
-		const std::set<std::string> own_recruits = current_team().recruits();
+	if (current_team().gold() < cheapest_unit_cost) {
+		DBG_AI_FLIX << "Not enough gold for recruiting \n";
+		return;  // This CA is going to be blacklisted for this turn.
+	}
 
 
 
-		/**
-		 * Step 1: Find important hexes and calculate other static things.
-		 * Maybe cache it for later use.
-		 * (TODO)
-		 */
+	/**
+	 * Step 1: Find important hexes and calculate other static things.
+	 * Maybe cache it for later use.
+	 * (TODO)
+	 */
 
-		/**
-		 * Step 2: Filter own_recruits according to configurations
-		 * (TODO)
-		 */
+	/**
+	 * Step 2: Filter own_recruits according to configurations
+	 * (TODO)
+	 */
 
-		score_map scores;
 
-		/**
-		 * Step 3: Fill scores with values coming from combat analysis and other stuff.
-		 */
+	/**
+	 * Step 3: Fill scores with values coming from combat analysis and other stuff.
+	 */
 
-		//For now just fill the scores with dummy entries:
-		//The first two units get a score of 1000, all others 0
-
+	// For now just fill the scores with dummy entries.
+	// The second and third unit get a score of 1000, all others 0
+	BOOST_FOREACH(data& data, leader_data) {
 		int idx = 0;
-		BOOST_FOREACH(std::string own_recruit, own_recruits){
-			scores[own_recruit] = (idx < 2) ? 1000 : 0;
-			idx++;
+		BOOST_FOREACH(score_map::value_type& entry, data.scores) {
+			const std::string& recruit = entry.first;
+
+			data.scores[recruit] = (idx > 0 && idx < 3) ? 1000 : 0;
+			++idx;
 		}
-
-
-		/**
-		 * Step 4: Do recruitment according to scores.
-		 * Note that the scores indicate the preferred mix to recruit but rather
-		 * the preferred mix of all units. So already existing units are considered.
-		 */
-
-		//Count all own units which are already on the map
-		std::map<std::string, int> own_units_count;
-		double own_units_total = 0;
-
-		const unit_map &units = *resources::units;
-
-		BOOST_FOREACH(const unit& unit, units){
-			if(unit.side() != current_team().side() || unit.can_recruit()){
-				continue;
-			}
-
-			++own_units_count[unit.type_name()];
-			++own_units_total;
-		}
-
-		//Calculate sum of scores
-		double scores_total = 0;
-		BOOST_FOREACH(score_map::value_type entry, scores){
-			double score = entry.second;
-
-			scores_total += score;
-		}
-		assert(scores_total > 0);
-
-		recruit_result_ptr recruit_result;
-		do{
-			std::string best_recruit;
-			double biggest_difference = -100;
-			BOOST_FOREACH(score_map::value_type entry, scores){
-				std::string unit = entry.first;
-				double score = entry.second;
-
-				double should_be = (score / scores_total) * 100; // %
-				double is = (own_units_total == 0) ? 0 : (own_units_count[unit] / own_units_total) * 100; // %
-				double difference = should_be - is;
-
-				if(difference > biggest_difference){
-					biggest_difference = difference;
-					best_recruit = unit;
-				}
-			}
-
-			recruit_result = check_recruit_action(best_recruit);
-			if(recruit_result->is_ok()){
-				recruit_result->execute();
-				LOG_AI_FLIX << "Recruited " << best_recruit << "\n";
-				++own_units_count[best_recruit];
-				++own_units_total;
-			}
-		} while(recruit_result->is_ok());
+		LOG_AI_FLIX << "\n" << data.to_string();
 	}
 
-	recruitment::~recruitment()
-	{
+	/**
+	 * Step 4: Do recruitment according to scores and the leaders ratio_scores.
+	 * Note that the scores don't indicate the preferred mix to recruit but rather
+	 * the preferred mix of all units. So already existing units are considered.
+	 */
+
+	// Count all own units which are already on the map
+	std::map<std::string, int> own_units_count;
+	int total_own_units = 0;
+
+	const unit_map &units = *resources::units;
+
+	BOOST_FOREACH(const unit& unit, units) {
+		if (unit.side() != get_side() || unit.can_recruit()) {
+			continue;
+		}
+
+		++own_units_count[unit.type_name()];
+		++total_own_units;
 	}
+
+
+	// the ratio_scores are there to decide which leader should recruit
+	// normalize them.
+	double ratio_score_sum = 0.0;
+	BOOST_FOREACH(const data& data, leader_data) {
+		ratio_score_sum += data.ratio_score;
+	}
+	assert(ratio_score_sum > 0.0);
+
+	BOOST_FOREACH(data& data, leader_data) {
+		data.ratio_score /= ratio_score_sum;
+	}
+
+
+	int total_recruit_count = 0;
+	recruit_result_ptr recruit_result;
+	do {
+		// find which leader should recruit according to ratio_scores
+		data* best_leader_data = NULL;
+		double biggest_difference = -100;
+		BOOST_FOREACH(const data& data, leader_data) {
+			double should_be = data.ratio_score * 100;  // %
+			double is = data.recruit_count;  // do in 2 steps to prevent int / int.
+			is = (total_recruit_count == 0) ? 0 : (is / total_recruit_count) * 100;  // %
+			double difference = should_be - is;
+			if (difference > biggest_difference) {
+				biggest_difference = difference;
+				best_leader_data = &data;
+			}
+		}
+		assert(best_leader_data);
+
+		// find which unit should this leader recruit according to best_leader_data.scores
+		std::string best_recruit;
+		biggest_difference = -100;
+		BOOST_FOREACH(const score_map::value_type& i, best_leader_data->get_normalized_scores()) {
+			const std::string& unit = i.first;
+			const double score = i.second;
+
+			double should_be = score * 100;  // %
+			double is = own_units_count[unit];  // do in 2 steps to prevent int / int.
+			is = (total_own_units == 0) ? 0 : (is / total_own_units) * 100;  // %
+			double difference = should_be - is;
+			if (difference > biggest_difference) {
+				biggest_difference = difference;
+				best_recruit = unit;
+			}
+		}
+
+		// TODO(flix): find the best hex for recruiting best_recruit.
+		// see http://forums.wesnoth.org/viewtopic.php?f=8&t=36571&p=526035#p525946
+		// "It also means there is a tendency to recruit from the outside in
+		// rather than the default inside out."
+		recruit_result = check_recruit_action(best_recruit,
+				map_location::null_location,
+				best_leader_data->leader.get_location());
+		if (recruit_result->is_ok()) {
+			recruit_result->execute();
+			LOG_AI_FLIX << "Recruited " << best_recruit << "\n";
+			++own_units_count[best_recruit];
+			++total_own_units;
+			++best_leader_data->recruit_count;
+			++total_recruit_count;
+			// TODO(flix): here something is needed to check if something changed in WML
+			// if yes just return. evaluate() and execute() will be called again.
+		} else {
+			// TODO(flix): here something is needed to decide if we may want to recruit a
+			// cheaper unit, when recruitment failed because of unit costs.
+		}
+	}while(recruit_result->is_ok());
 }
 
-}
+recruitment::~recruitment() { }
+
+}  // namespace flix_recruitment
+}  // namespace ai
--- src/ai/flix/recruitment.hpp
+++ src/ai/flix/recruitment.hpp
@@ -13,21 +13,21 @@
 */
 
 /**
- * Experimental Recruitment Engine by Flix
- * Uses Game Theory to find the best mix to recruit
- * when only the enemies recruitment-list is available.
- *
- * See http://wiki.wesnoth.org/User:Flixx/Game_Theory_for_Recruiting
- * for a full explanation
  * @file
+ * Recruitment Engine by flix
  */
 
 #ifndef AI_FLIX_RECRUITMENT_HPP_INCLUDED
 #define AI_FLIX_RECRUITMENT_HPP_INCLUDED
 
+#include "../composite/rca.hpp"
+#include "../../unit.hpp"
+
+#include <boost/foreach.hpp>
+
 #ifdef _MSC_VER
 #pragma warning(push)
-//silence "inherits via dominance" warnings
+// silence "inherits via dominance" warnings
 #pragma warning(disable:4250)
 #endif
 
@@ -35,19 +35,76 @@ namespace ai {
 
 namespace flix_recruitment {
 
+// each leader have a score_map
+// the score map indicates what is the best mix of own units from the leader point of view.
+// The leader will then recruit according to the map.
 typedef std::map<std::string, double> score_map;
+
+// In the limit_map it is stored how much units we can recruit per unit type.
+// Those limits come from configurations.
+// Each leader will have a limit_map and there will also be a global limit_map for both leader.
+// This is just a dummy yet and will depend on the configuration definitions.
+typedef std::map<std::string, int> limit_map;
+
+struct data {
+	unit leader;
+	std::set<std::string> recruits;
+	score_map scores;
+	limit_map limits;
+
+	// We use ratio_score to decide with which ratios the leaders recruit among each other.
+	// For example if leader1 have a ratio_score of 1 and leader2 have a ratio_score of 2
+	// then leader2 will recruit twice as much units than leader1.
+	double ratio_score;
+
+	int recruit_count;
+
+	explicit data(const unit& leader)
+		: leader(leader), ratio_score(1.0), recruit_count(0) { }
+	double get_score_sum() const {
+		double sum = 0.0;
+		BOOST_FOREACH(const score_map::value_type& entry, scores) {
+			sum += entry.second;
+		}
+		return sum;
+	}
+	score_map get_normalized_scores() const {
+		const double sum = get_score_sum();
+		if (sum == 0.0) {
+			return scores;
+		}
+		score_map normalized;
+		BOOST_FOREACH(const score_map::value_type& entry, scores) {
+			normalized[entry.first] = entry.second / sum;
+		}
+		return normalized;
+	}
+	std::string to_string() {
+		std::stringstream s;
+		s << "---------Content of leader data----------\n";
+		s << "For leader: " << leader.name() << "\n";
+		s << "ratio_score: " << ratio_score << "\n";
+		s << "recruit_count: " << recruit_count << "\n";
+		BOOST_FOREACH(const score_map::value_type& entry, scores) {
+			s << entry.first << " ---- score: " << entry.second <<
+					" limit: " << limits[entry.first] << "\n";
+		}
+		s << "-----------------------------------------\n";
+		return s.str();
+	}
+};
 
 class recruitment : public candidate_action {
 public:
-	recruitment( rca_context &context , const config &cfg );
+	recruitment(rca_context &context, const config &cfg);
 	virtual ~recruitment();
 	virtual double evaluate();
 	virtual void execute();
 };
 
-} // of namespace testing_ai_default
+}  // of namespace flix_recruitment
 
-} // of namespace ai
+}  // of namespace ai
 
 #ifdef _MSC_VER
 #pragma warning(pop)
